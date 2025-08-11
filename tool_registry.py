b59961d2a31f38fc144b4b97223fad8ed8627b31
dengguoji <jdg777111@gmail.com>
2025-08-11 15:45:31 +0800

    工具注册与执行

--- tool_registry.py
+++ tool_registry.py
@@ -1,11 +1,20 @@
+from tools.get_attendance_rate import get_attendance_rate
 from tools.weather import get_weather
 from tools.time import get_current_time
-from tools.meeting import meeting
+from tools.create_live import create_live
 from tools.notice import notice
+from tools.publish_discussion import publish_discussion
+from tools.create_practice_task import create_practice_task
+from tools.get_components_property import get_components_property
+
 
 FUNCTION_REGISTRY = {
     "get_weather": get_weather,
     "get_current_time": get_current_time,
-    "meeting": meeting,
-    "notice": notice
+    "create_live": create_live,
+    "notice": notice,
+    "get_attendance_rate":get_attendance_rate,
+    "publish_discussion": publish_discussion,
+    "create_practice_task": create_practice_task,
+    "get_components_property": get_components_property
 }
