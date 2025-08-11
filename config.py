66fcddd92a0269e0a5ba63f1f82de36b85608212
dengguoji <jdg777111@gmail.com>
2025-08-11 15:42:45 +0800

    tool加入TOOLS

--- config.py
+++ config.py
@@ -2,11 +2,12 @@
 import time
 import os
 
-# API KEY
+# 方舟API KEY
 API_KEY = os.getenv("ARK_API_KEY")
 
-# 模型接入点
-MODEL_NAME = ""  # doubao-1.5-pro-32k
+# 方舟模型节点
+MODEL_NAME = "ep-20250611123221-wnqgm"  # seed 1.6 256K上下文 16K输出 disable reasoning
+MODEL_NAME_REASONING = "ep-20250711162541-6znl7"  # seed 1.6 256K上下文 16K输出
 
 # Function Call的tools定义
 TOOLS = [
@@ -21,7 +22,7 @@ TOOLS = [
                 "properties": {
                     "city": {
                         "type": "string",
-                        "description": "城市名称，比如北京、上海",
+                        "description": "城市名称，比如北京、上海，默认北京市",
                     }
                 },
                 "required": ["city"],
@@ -48,26 +49,26 @@ TOOLS = [
         }
     }
     ,
-    # 创建会议
+    # 创建直播
     {
         "type": "function",
         "function": {
-            "name": "meeting",
+            "name": "create_live",
             "description": "创建直播,需要给入直播的标题、开始时间和结束时间",
             "parameters": {
                 "type": "object",
                 "properties": {
                     "title": {
                         "type": "string",
-                        "description": "会议标题"
+                        "description": "直播标题"
                     },
                     "start_time": {
                         "type": "string",
-                        "description": "会议开始时间，格式为YYYY-MM-DD HH:MM:SS"
+                        "description": "直播开始时间，格式为YYYY-MM-DD HH:MM:SS"
                     },
                     "end_time": {
                         "type": "string",
-                        "description": "会议结束时间，格式为YYYY-MM-DD HH:MM:SS"
+                        "description": "直播结束时间，格式为YYYY-MM-DD HH:MM:SS"
                     }
                 },
                 "required": ["title", "start_time", "end_time"]
@@ -86,7 +87,7 @@ TOOLS = [
                 "properties": {
                     "object": {
                         "type": "string",
-                        "description": "通知对象"
+                        "description": "通知对象，默认全部班级"
                     },
                     "title": {
                         "type": "string",
@@ -100,5 +101,114 @@ TOOLS = [
                 "required": ["object","title", "content"]
             }
         }
+    },
+    # 查询签到率
+    {
+        "type": "function",
+        "function": {
+            "name": "get_attendance_rate",
+            "description": "查询某院系在指定时间段内的签到率,单次只能查询一个院系且一个时间段",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "start_date": {
+                        "type": "string",
+                        "description": "开始日期，格式为YYYY-MM-DD"
+                    },
+                    "end_date": {
+                        "type": "string",
+                        "description": "结束日期，格式为YYYY-MM-DD"
+                    },
+                    "department_id": {
+                        "type": "string",
+                        "description": "院系ID"
+                    }
+                },
+                "required": ["start_date", "end_date", "department_id"]
+            }
+        }
+    },
+    # 发布讨论
+    {
+        "type": "function",
+        "function": {
+            "name": "publish_discussion",
+            "description": "发布讨论，需要给入讨论的标题和内容以及发布对象",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "title": {
+                        "type": "string",
+                        "description": "讨论标题"
+                    },
+                    "content": {
+                        "type": "string",
+                        "description": "讨论内容"
+                    },
+                    "target": {
+                        "type": "string",
+                        "description": "发布对象，比如某个班级或院系，默认全部班级"
+                    }
+
+                },
+                "required": ["title", "content","target"]
+            }
+        }
+    },
+    # 创建实践任务
+    {
+        "type": "function",
+        "function": {
+            "name": "create_practice_task",
+            "description": "创建实践任务，需要给入任务的标题、内容、发布对象和截止时间",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "title": {
+                        "type": "string",
+                        "description": "实践任务标题"
+                    },
+                    "content": {
+                        "type": "string",
+                        "description": "实践任务内容"
+                    },
+                    "target": {
+                        "type": "string",
+                        "description": "实践任务发布对象，比如某个班级或院系，默认全部班级"
+                    },
+                    "deadline": {
+                        "type": "string",
+                        "description": "实践任务截止时间，格式为YYYY-MM-DD HH:MM:SS"
+                    }
+                },
+                "required": ["title", "content","target","deadline"]
+            }
+        }
     }
+
+]
+
+# ccdx
+TOOLS_CCDX = [
+    # 获取元件属性
+    {
+    "type": "function",
+    "function": {
+        "name": "get_components_property",
+        "description": "根据一个或多个元件名称,返回对应元件的属性定义",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "names": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    },
+                    "description": "要查询的元件名称列表，例如 [\"Cell\", \"Lamp\"]"
+                }
+            },
+            "required": ["names"]
+        }
+    }
+}
 ]
